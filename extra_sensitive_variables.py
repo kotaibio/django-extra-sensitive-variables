da3add73d11c974b037c2c88332534c6ef85109b
John Nieri <nieri@kotai-bio.com>
2017-09-27 14:27:12 +0900

    traceback and post parameter substring detection

--- extra_sensitive_variables.py
+++ extra_sensitive_variables.py
@@ -3,17 +3,20 @@ from django.views.debug import SafeExceptionReporterFilter, CLEANSED_SUBSTITUTE
 
 
 class ExtraSensitiveReporterFilter(SafeExceptionReporterFilter):
-
     def get_post_parameters(self, *args, **kwargs):
         cleansed = super(ExtraSensitiveReporterFilter, self).get_post_parameters(*args, **kwargs).copy()
-        for param in settings.EXTRA_SENSITIVE_POST_PARAMETERS:
-            if param in cleansed:
-                cleansed[param] = CLEANSED_SUBSTITUTE
+        for param_substring in settings.EXTRA_SENSITIVE_POST_PARAMETERS:
+            for param in cleansed:
+                if param_substring in param:
+                    cleansed[param] = CLEANSED_SUBSTITUTE
+                    break
         return cleansed
 
     def get_traceback_frame_variables(self, *args, **kwargs):
         cleansed = dict(super(ExtraSensitiveReporterFilter, self).get_traceback_frame_variables(*args, **kwargs))
-        for param in settings.EXTRA_SENSITIVE_VARIABLES:
-            if param in cleansed:
-                cleansed[param] = CLEANSED_SUBSTITUTE
+        for param_substring in settings.EXTRA_SENSITIVE_VARIABLES:
+            for param in cleansed:
+                if param_substring in param:
+                    cleansed[param] = CLEANSED_SUBSTITUTE
+                    break
         return cleansed.items()
